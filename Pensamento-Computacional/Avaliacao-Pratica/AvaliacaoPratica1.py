ddf046b1654ce500376505ea902c7f2312c281e4
FelipeCeub <felipe.amorim@tutamail.com>
2024-08-22 21:20:27 -0300

    Adicionando Avaliações

---
+++ Pensamento-Computacional/Avaliacao-Pratica/AvaliacaoPratica1.py
@@ -0,0 +1,10 @@
+def AvaliacaoPratica1A():
+#A)Implemente o programa que calcule o volume de uma esfera de raio R. O usuário fornecerá o dado necessário.
+    
+    raio = float(input('Digite o raio R : '))
+    volume = (4/3) * 3.14 * (raio**2)
+
+    print(f"O volume da efera de raio R {raio} é igual à: {volume:.2f}")
+
+def AvaliacaoPratica1B():
+#B)
